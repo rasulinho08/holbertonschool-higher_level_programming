e2665c8dab4ee9de72ce7bd3208a0370b59ec3b4
Rasul028 <mamishovrasul028@gmail.com>
2025-01-28 15:25:34 +0400

    Update 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     # Ensure both tuples have at least 2 elements by padding with 0s if necessary
     a1 = tuple_a[0] if len(tuple_a) > 0 else 0
