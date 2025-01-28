d8a1102cca812030922ab2c3b6fd2bce54d6ace5
Rasul028 <mamishovrasul028@gmail.com>
2025-01-28 14:19:10 +0400

    9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+def max_integer(my_list=[]):
+    if not my_list:  # Check if the list is empty
+        return None
+
+    # Initialize the first element as the maximum
+    max_val = my_list[0]
+
+    # Iterate through the list to find the maximum
+    for num in my_list:
+        if num > max_val:
+            max_val = num
+
+    return max_val
+
