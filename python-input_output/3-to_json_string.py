daabda20d64573869a608f206acbf27109875b7a
rasulinho08 <mamishovrasul028@gmail.com>
2025-02-25 11:30:04 +0000

    w2i

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-"""This module to_json_string() file"""
 import json
 
-
 def to_json_string(my_obj):
-    """
-    Returns the JSON representation of an object (string)
-
-    Parameters
-    ----------
-       my_obj : object
-
-    Return
-    ------
-        str
-            The JSON representation of an object (string)
-    """
-    return json.dumps(my_obj)                                                                                             
+    """Returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)                                                                                            
 ~                                                 
