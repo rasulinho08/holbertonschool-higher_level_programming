6b2b2907402c815ced93d93a9290cbe9ad476299
Rasul028 <mamishovrasul028@gmail.com>
2025-03-31 13:35:52 +0000

    fmk

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
+    """Updates or adds a key-value pair in a dictionary."""
     a_dictionary[key] = value
-    return a_dictionary 
+    return a_dictionary
+ 
