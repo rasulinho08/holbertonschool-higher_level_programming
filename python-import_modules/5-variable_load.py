80c1bc8b72b1cdc3669b621aca4516d837c6415d
rasulinho08 <mamishovrasul028@gmail.com>
2025-12-09 14:45:58 +0000

    Make executable

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+# 5-variable_load.py
+"""
+Imports the variable 'a' from variable_load_5.py
+and prints its value.
+"""
+from variable_load_5 import a
 
 if __name__ == "__main__":
-    from variable_load_5 import a
     print(a)
